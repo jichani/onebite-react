d49ccffdf0d64ccaa7552528d830b2c06b6b38e9
Jichani <123962013+jichani@users.noreply.github.com>
2024-05-01 16:03:55 +0900

    이벤트 핸들러 공부

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 // 구조분해 할당
 export default function Button({ text, color, children }) {
+  const onClickButton = () => {
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button onClick={onClickButton} style={{ color: color }}>
       {text} - {color.toUpperCase()}
       {children}
     </button>
