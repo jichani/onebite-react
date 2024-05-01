e82e1d4867f6cb86a7f58f4b78602ac715daafcf
Jichani <123962013+jichani@users.noreply.github.com>
2024-05-01 16:24:59 +0900

    useState

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,32 @@
+import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} /> {/* 스프레드 연산자 */}
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
