4ac3bddbb53228c7a5029f75aa008671c7a0646d
Jichani <123962013+jichani@users.noreply.github.com>
2024-05-13 23:23:46 +0900

    8.3까지 완료

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+import Header from "./components/Header";
+import List from "./components/List";
+
+const tempData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
-  return <>TodoList</>;
+  const [todos, setTodos] = useState(tempData);
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
---
+++ section08/src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+
+export default function List() {
+  return (
+    <div className="List">
+      <h4>Todo List 🎶</h4>
+      <input placeholder="검색어를 입력하세요" />
+      <div className="todos_wrapper">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+}
